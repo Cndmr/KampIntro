ba9e2e14d8ec18e8e296903ac4802e9405fe6212
Can Demir <84686864+Cndmr@users.noreply.github.com>
2023-05-22 17:48:41 +0300

    MüşteriBilgisi

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Name +"Müşteri Eklendi");
+        }
+        public void Sil (Musteri musteri)
+        {
+            Console.WriteLine(musteri.Name +"Müşteri Silindi");
+        }
+        public void Listele(Musteri[] musteriler)
+         
+        {
+            Console.WriteLine("Müşteri Listesi");
+            foreach (var item in musteriler)
+            {
+                Console.WriteLine(item.Name + item.Surname + item.PhoneNumber);
+
+            }
+
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClassMetotDemo;
+
+Musteri musteri1 = new Musteri();
+musteri1.Name = "Oğuz";
+musteri1.Surname = "Demirel";
+musteri1.PhoneNumber = "05556667799" ;
+
+Musteri musteri2 = new Musteri();
+musteri2.Name = "can";
+musteri2.Surname = "demir";
+musteri2.PhoneNumber = "05556667788";
+
+Musteri musteri3 = new Musteri();
+musteri3.Name = "Burak";
+musteri3.Surname = "İpekçi";
+musteri3.PhoneNumber = "05556667710";
+
+MusteriManager musteriManager = new MusteriManager();
+musteriManager.Ekle(musteri1);
+musteriManager.Ekle(musteri2);
+musteriManager.Ekle(musteri3);
+musteriManager.Sil(musteri1);
+musteriManager.Sil(musteri2);
+musteriManager.Sil(musteri3);
+Console.WriteLine("----------------------------------");
+Musteri[] musteris = new Musteri[] { musteri1, musteri2, musteri3 };
+musteriManager.Listele(musteris);
