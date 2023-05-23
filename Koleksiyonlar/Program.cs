d82e50d1f8c6d5f38f6ce71648019648019604a6
Can Demir <84686864+Cndmr@users.noreply.github.com>
2023-05-23 18:29:50 +0300

    23MayısDersSonu

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] isimler = new string[]
+{
+    "Engin", "Can", "Murat", "Oğuz"};
+
+Console.WriteLine(isimler[0]);
+Console.WriteLine(isimler[1]);
+Console.WriteLine(isimler[2]);
+Console.WriteLine(isimler[3]);
+List<string> isimler2 = new List<string> { "engin", "murat", "can", "oğuz" };
+Console.WriteLine(isimler2[0]);
+Console.WriteLine(isimler2[1]);
+Console.WriteLine(isimler2[2]);
+Console.WriteLine(isimler2[3]);
+
--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOP1;
+
+Product product1 = new();
+product1.Id = 1;
+product1.CategoryId = 2;
+product1.ProductName = "Masa";
+product1.UnitPrice = 500;
+product1.UnitsInStock = 3;
+
+Product product2 = new Product 
+{
+    Id = 2, 
+    CategoryId = 5, 
+    ProductName = "Kalem", 
+    UnitPrice = 20, 
+    UnitsInStock = 5 
+};
+
+ProductManager productManager = new();
+productManager.Add(product1);
+Console.WriteLine(product1.ProductName);
