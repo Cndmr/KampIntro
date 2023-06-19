5e27e978436ce5f9f418d4cf6e294ee09ee99f11
Can Demir <84686864+Cndmr@users.noreply.github.com>
2023-06-19 19:07:20 +0300

    LoggerService,Interface,Inheritance, Business Classes,Method Injection

---
+++ OOP2/Program.cs
@@ -0,0 +1,18 @@
+﻿using OOP2;
+
+GercekMusteri musteri1 = new();
+musteri1.Id = 1;
+musteri1.MusteriNo = "12345";
+musteri1.Adi = "Can";
+musteri1.Soyadi = "Demir";
+musteri1.TcNo = "29200998331";
+
+TuzelMusteri musteri2 = new();
+musteri2.Id = 2;
+musteri2.MusteriNo="123";
+musteri2.SirketAdi = "Demir Yazılım";
+musteri2.VergiNo = "16223";
+
+MusteriManager musteriManager=new MusteriManager();
+musteriManager.Ekle(musteri1);
+musteriManager.Ekle(musteri2);
---
+++ OOP3/Program.cs
@@ -0,0 +1,16 @@
+﻿using OOP3;
+using System.Collections.Generic;
+
+IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+IKrediManager tasitKrediManager = new TasitKrediManager();
+IKrediManager konutKrediManager = new KonutKrediManager();
+
+BasvuruManager basvuruManager = new BasvuruManager();
+
+ILoggerService databaseLoggerService = new DatabaseLoggerService();
+ILoggerService fileLoggerService = new FileLoggerService();
+
+basvuruManager.BasvuruYap(konutKrediManager, databaseLoggerService) ;
+
+List<IKrediManager> krediler =new List<IKrediManager>() {ihtiyacKrediManager, konutKrediManager, tasitKrediManager };
+//basvuruManager.KrediOnBilgilendirmesiYap(krediler);
