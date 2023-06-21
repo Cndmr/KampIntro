990531f5b297b1be931dca8792fb0331d1c067fd
Can Demir <84686864+Cndmr@users.noreply.github.com>
2023-06-21 18:19:41 +0300

    RecapDemo Checkboard Making

---
+++ Classes/Program.cs
@@ -0,0 +1,17 @@
+﻿using Classes;
+
+CustomerManager customerManager = new CustomerManager();
+customerManager.Add();
+customerManager.Updated();
+
+ProductManager productManager = new ProductManager();
+productManager.Add();
+productManager.Updated();
+
+Customer customer = new Customer();
+customer.Id = 1;
+customer.Name = "Can";
+customer.LastName = "Demir";
+customer.City = "İstanbul";
+
+Console.WriteLine(customer.Name);
