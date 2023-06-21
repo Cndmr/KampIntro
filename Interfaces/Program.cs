f8dd4e6bd98904824c56a60675cfaa6652b84f89
Can Demir <84686864+Cndmr@users.noreply.github.com>
2023-06-21 18:54:53 +0300

    Interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,18 @@
+﻿interface IPersons
+{
+    int Id { get; set; }
+    string Name { get; set; }
+    string LastName { get; set; }
+}
+class Customer : IPersons
+{
+    public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+}
+class Student : IPersons
+{
+    public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+}
