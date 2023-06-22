6c95f13a42fe5d4a1e5135070c1c7fc28ab79049
Can Demir <84686864+Cndmr@users.noreply.github.com>
2023-06-22 19:04:35 +0300

    Inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,36 @@
+﻿
+Person[] persons = new Person[3]
+{
+    new Customer
+    {
+        Name="Can"
+    },
+    new Student
+    {
+        StudentNo=1
+    },
+new Person
+{
+    Name="Bla"
+} 
+};
+foreach (var person in persons)
+{
+    Console.WriteLine( person.Name );
+}
+Console.ReadLine();
+class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+}
+class Customer : Person
+{
+    public string City { get; set; }
+}
+
+class Student : Person
+{
+    public int StudentNo { get; set; }
+}
